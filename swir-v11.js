34abef46b111e099bee4d7f91c252ad104a394ca
Wojciech <swirjestem@hotmail.com>
2026-09-16 17:06:24 +0200

    Load bundled system application locale pack

--- swir-v11.js
+++ swir-v11.js
@@ -5,7 +5,8 @@
     './swir-app-locales-west.js?v=0.1.0',
     './swir-app-locales-global.js?v=0.1.0',
     './swir-app-locales-world.js?v=0.1.0',
-    './swir-app-i18n-host.js?v=0.1.0'
+    './swir-app-locales-system.js?v=0.2.0',
+    './swir-app-i18n-host.js?v=0.2.0'
   ];
   for (const src of queue) {
     const script = document.createElement('script');
